dbab998f23b4c622fde4da5d438c4d4e7193d41a
Bendegúz Tóth <toth.bendeguz@students.jedlik.eu>
2025-02-12 11:28:44 +0100

    alap generálása

---
+++ EtteremSimulator/EtteremSimulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EtteremSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

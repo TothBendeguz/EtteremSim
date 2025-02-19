8be18e8fbc6f27e30deb900aa86f19b7f6a91101
Bendegúz Tóth <toth.bendeguz@students.jedlik.eu>
2025-02-19 08:36:29 +0100

    Pincer.cs

--- EtteremSimulator/EtteremSimulator/Pincer.cs
+++ EtteremSimulator/EtteremSimulator/Pincer.cs
@@ -8,5 +8,44 @@ namespace EtteremSimulator
 {
     internal class Pincer
     {
+        public int Szam { get; }
+        private Asztal[] rendelesek;
+        private int rendelesekSzama;
+
+        public Pincer(int szam)
+        {
+            Szam = szam;
+            rendelesek = new Asztal[10];
+            rendelesekSzama = 0;
+        }
+
+        public void RendelesFelvesz(Asztal asztal)
+        {
+            if (rendelesekSzama < rendelesek.Length)
+            {
+                rendelesek[rendelesekSzama] = asztal;
+                rendelesekSzama++;
+            }
+        }
+
+        public Asztal RendelesLead()
+        {
+            if (rendelesekSzama > 0)
+            {
+                Asztal asztal = rendelesek[0];
+                for (int i = 1; i < rendelesekSzama; i++)
+                {
+                    rendelesek[i - 1] = rendelesek[i];
+                }
+                rendelesekSzama--;
+                return asztal;
+            }
+            return null;
+        }
+
+        public int RendelesekSzama()
+        {
+            return rendelesekSzama;
+        }
     }
 }

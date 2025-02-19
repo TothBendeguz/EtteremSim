eddeda3e02b16d44c4a305cd4cc9649174b515cb
Bendegúz Tóth <toth.bendeguz@students.jedlik.eu>
2025-02-19 08:36:48 +0100

    Vendeg.cs

--- EtteremSimulator/EtteremSimulator/Vendeg.cs
+++ EtteremSimulator/EtteremSimulator/Vendeg.cs
@@ -8,5 +8,28 @@ namespace EtteremSimulator
 {
     internal class Vendeg
     {
+        public int Szam { get; }
+        public Etel Rendeles { get; private set; }
+        public bool Rendelt { get; private set; }
+        public bool EtelFogyasztva { get; private set; }
+        public int Elegedettseg { get; private set; }
+
+        public Vendeg(int szam)
+        {
+            Szam = szam;
+            Elegedettseg = 100;
+        }
+
+        public void Rendel(Etel etel)
+        {
+            Rendeles = etel;
+            Rendelt = true;
+        }
+
+        public void EtelFogyaszt()
+        {
+            EtelFogyasztva = true;
+            Elegedettseg -= new Random().Next(0, 20);
+        }
     }
 }

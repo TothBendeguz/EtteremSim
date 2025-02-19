1a8421894891b21870348e8d08d0732d5ef1f89e
Bendegúz Tóth <toth.bendeguz@students.jedlik.eu>
2025-02-19 08:36:12 +0100

    Etel.cs

--- EtteremSimulator/EtteremSimulator/Etel.cs
+++ EtteremSimulator/EtteremSimulator/Etel.cs
@@ -8,5 +8,13 @@ namespace EtteremSimulator
 {
     internal class Etel
     {
+        public string Nev { get; }
+        public int ElkeszitesiIdo { get; }
+
+        public Etel(string nev, int elkeszitesiIdo)
+        {
+            Nev = nev;
+            ElkeszitesiIdo = elkeszitesiIdo;
+        }
     }
 }
